d44d266cd220c19fc2009e56d86e322dfd5f9943
Rafael Milewski <rafael.milewski@gmail.com>
2023-10-01 12:56:14 +0800

    Add column toggler support (#21)

--- resources/js/components/CustomRelationshipField.vue
+++ resources/js/components/CustomRelationshipField.vue
@@ -6,7 +6,7 @@
         :via-resource="resourceName"
         :via-resource-id="resourceId"
         :via-relationship="null"
-        :relationship-type="`CustomRelationshipField:${ encodedAttribute }`"
+        :relationship-type="relationshipType"
         @actionExecuted="actionExecuted"
         :load-cards="false"
         :initial-per-page="field.perPage || 5"
@@ -27,11 +27,27 @@
                 this.$emit('actionExecuted')
             },
         },
+        data() {
+            return {
+                eventCallback: null,
+            }
+        },
+        mounted() {
+            Nova.$on('custom-relationship-field:request-extra-params', this.eventCallback = () => {
+                Nova.$emit('custom-relationship-field:extra-params', { relationshipType: this.relationshipType })
+            })
+        },
+        unmounted() {
+            Nova.$off('custom-relationship-field:request-extra-params', this.eventCallback)
+        },
         computed: {
             encodedAttribute() {
                 return btoa(`${ this.field.attribute }|_::_|${ this.field.name }`)
             },
-        }
+            relationshipType() {
+                return `CustomRelationshipField:${ this.encodedAttribute }`
+            },
+        },
     }
 
 </script>
